22f4449fcb4168ebc67ef94b111a5d434c186861
Nanqing <nanqing.dong@gmail.com>
2016-07-22 19:50:07 -0400

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,244 @@
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 1200 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
+
+var projection = d3.geo.mercator();
+var path = d3.geo.path().projection(projection);
+
+var svg = d3.select("#map1").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+var g = svg.append("g");
+
+var digitScale = d3.scale.category20b();
+
+var isoidData;
+
+//read json file
+d3.json("json/world-50m.json", function(error, world) {
+  //worldObjects = world;
+
+  if(error) {
+    console.log(error);
+  }
+
+  var countries = topojson.feature(world,world.objects.countries).features;
+
+    //add shadow to stroke
+  var filter = g.append("filter")
+        .attr("id", "stroke-shadow")
+        .attr("height", "120%");
+
+      filter.append("feGaussianBlur")
+          .attr("in", "SourceAlpha")
+          .attr("stdDeviation", 1)
+          .attr("result", "blur");
+
+      filter.append("feOffset")
+          .attr("in", "blur")
+          .attr("dx", 0.1)
+          .attr("dy", 0.1)
+          .attr("result", "offsetBlur");
+
+    var feMerge = filter.append("feMerge");
+
+    feMerge.append("feMergeNode").attr("in", "offsetBlur");
+    feMerge.append("feMergeNode").attr("in", "SourceGraphic");
+
+      var filter2 = g.append("filter")
+        .attr("id", "stroke-shadow2")
+        .attr("height", "120%");
+
+      filter2.append("feGaussianBlur")
+          .attr("in", "SourceAlpha")
+          .attr("stdDeviation", 2)
+          .attr("result", "blur");
+
+      filter2.append("feOffset")
+          .attr("in", "blur")
+          .attr("dx", 0.5)
+          .attr("dy", 1)
+          .attr("result", "offsetBlur");
+
+    var feMerge2 = filter2.append("feMerge");
+
+    feMerge2.append("feMergeNode").attr("in", "offsetBlur");
+    feMerge2.append("feMergeNode").attr("in", "SourceGraphic");
+
+    var filter3 = g.append("filter")
+        .attr("id", "stroke-shadow3")
+        .attr("height", "140%");
+
+      filter3.append("feGaussianBlur")
+          .attr("in", "SourceAlpha")
+          .attr("stdDeviation", 2)
+          .attr("result", "blur");
+
+      filter3.append("feOffset")
+          .attr("in", "blur")
+          .attr("dx", 0.8)
+          .attr("dy", 1.5)
+          .attr("result", "offsetBlur");
+
+    var feMerge3 = filter3.append("feMerge");
+
+    feMerge3.append("feMergeNode").attr("in", "offsetBlur");
+    feMerge3.append("feMergeNode").attr("in", "SourceGraphic");
+
+    var filter4 = g.append("filter")
+        .attr("id", "stroke-shadow4")
+        .attr("height", "140%");
+
+      filter4.append("feGaussianBlur")
+          .attr("in", "SourceAlpha")
+          .attr("stdDeviation", 2)
+          .attr("result", "blur");
+
+      filter4.append("feOffset")
+          .attr("in", "blur")
+          .attr("dx", 1.1)
+          .attr("dy", 2.5)
+          .attr("result", "offsetBlur");
+
+    var feMerge4 = filter4.append("feMerge");
+
+    feMerge4.append("feMergeNode").attr("in", "offsetBlur");
+    feMerge4.append("feMergeNode").attr("in", "SourceGraphic");
+
+
+    g.selectAll("path").data(countries).enter()
+    .append("path").attr("d",path)
+    .style("stroke","#fff")
+    .style("stroke-width","1")
+    .style("stroke-opacity","1")
+    .style("stroke-linejoin","round");
+
+  d3.csv("data/WomenInWorkforce.csv",function(error,data){ //WomenInWorkforce.csv
+    if (error) {console.log(error);}
+    isoidData = d3.map(data, function (county) { return Number(county.ISOID); //console.log(county.ISOID)
+    });
+    g.selectAll("path")
+    .style("fill",function(d){
+      var countryData = isoidData.get(d.id);//only get one id
+      if (!countryData) { return "#DDDDDD";}
+      else if (countryData.LaborForce <= 20) { return "#feebe2";}
+      else if (countryData.LaborForce > 20 && countryData.LaborForce <=40) { return "#fbb4b9";}
+      else if (countryData.LaborForce > 40 && countryData.LaborForce <=60) { return "#f768a1";}
+      else if (countryData.LaborForce > 60 && countryData.LaborForce <=80) { return "#c51b8a";}
+      else if (countryData.LaborForce > 80 && countryData.LaborForce <=100) { return "#7a0177";}
+
+      return "#DDDDDD";
+    })
+    .style("filter",function(d){
+      var countryData = isoidData.get(d.id);
+      if (!countryData) { return false;}
+      else if (countryData.LaborForce <=20) {return "url(#stroke-shadow4)";}
+      else if (countryData.LaborForce > 20 && countryData.LaborForce <=40) { return "url(#stroke-shadow3)";}
+      else if (countryData.LaborForce > 40 && countryData.LaborForce <=60) { return "url(#stroke-shadow2)";}
+      else if (countryData.LaborForce > 60 && countryData.LaborForce <=80) { return "url(#stroke-shadow2)";}
+      else if (countryData.LaborForce > 80 && countryData.LaborForce <=100) { return "url(#stroke-shadow)";}
+
+
+
+    });
+  });
+
+});
+
+//explanations
+svg.append("text")
+    .attr("x",width + 50)
+    .attr("y",5)
+    .attr("dy",".35em")
+    .style("text-anchor", "end")
+    .style("font-weight", "bold")
+    .text("Female Labor Force Participation");
+
+svg.append("rect")
+      .attr("x", width + 41)
+       .attr("y",15)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", "#7a0177")
+svg.append("text")
+    .attr("x",width + 28)
+    .attr("y",9+15)
+    .attr("dy",".35em")
+    .style("text-anchor", "end")
+    .text("80% - 100%");
+
+svg.append("rect")
+      .attr("x", width + 41)
+      .attr("y",18+2+15)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", "#c51b8a");
+svg.append("text")
+    .attr("x",width + 28)
+    .attr("y",9+20+15)
+    .attr("dy",".35em")
+    .style("text-anchor", "end")
+    .text("60% - 80%");
+
+svg.append("rect")
+      .attr("x", width + 41)
+      .attr("y",18+2+20+15)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", "#f768a1");
+svg.append("text")
+    .attr("x",width + 28)
+    .attr("y",9+40+15)
+    .attr("dy",".35em")
+    .style("text-anchor", "end")
+    .text("40% - 60%");
+
+svg.append("rect")
+      .attr("x", width + 41)
+      .attr("y",18+2+20+20+15)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", "#fbb4b9");
+svg.append("text")
+    .attr("x",width + 28)
+    .attr("y",9+60+15)
+    .attr("dy",".35em")
+    .style("text-anchor", "end")
+    .text("20% - 40%");
+
+svg.append("rect")
+      .attr("x", width + 41)
+      .attr("y",18+2+20+20+20+15)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", "#feebe2");
+svg.append("text")
+    .attr("x",width + 28)
+    .attr("y",9+80+15)
+    .attr("dy",".35em")
+    .style("text-anchor", "end")
+    .text("0% - 20%");
+
+svg.append("rect")
+      .attr("x", width + 41)
+      .attr("y",18+2+20+20+20+20+15)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", "#DDDDDD");
+svg.append("text")
+    .attr("x",width + 28)
+    .attr("y",9+100+15)
+    .attr("dy",".35em")
+    .style("text-anchor", "end")
+    .text("Data Unavailable");
+
+//title
+/*svg.append("text")
+    .style("fill","#4F598C")
+    .style("font-size","18px")
+    .attr("x",370)
+    .attr("y",9)
+    .attr("dy",".65em")
+    .style("text-anchor", "end")
+    .text("Map of Female Labor Force Participation(%)");
+*/
---
+++ js/map2.js
@@ -0,0 +1,179 @@
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 1056 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
+
+// setup x
+var xValue = function(d) { return d.literacyRate;}, // data -> value
+    xScale = d3.scale.linear().domain([0,100]).range([0, width - 300]), // value -> display
+    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(11);
+
+// setup y
+var yValue = function(d) { return d.LFPrate;}, // data -> value
+    yScale = d3.scale.linear().domain([0,100]).range([height, 0]), // value -> display
+    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+    yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
+
+// setup fill color
+var color = function(d) {
+      if (d.LFPrate <= 20 && d.state == "dark") { return "#feebe2";}
+      else if ( d.state == "dark" && d.LFPrate > 20 && d.LFPrate <=40) { return "#fbb4b9";}
+      else if (d.state == "dark" && d.LFPrate > 40 && d.LFPrate <=60) { return "#f768a1";}
+      else if (d.state == "dark" && d.LFPrate > 60 && d.LFPrate <=80) { return "#c51b8a";}
+      else if (d.state == "dark" && d.LFPrate > 80 && d.LFPrate <=100) { return "#7a0177";}
+      return "#bdbdbd";
+}
+
+
+
+var cValue = function(d) { return d.developState;};
+
+// add the graph canvas to the body of the webpage
+var svg = d3.select("#map2").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// add the tooltip area to the webpage
+var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+
+// load data
+d3.csv("data/mergedData.csv", function(error, data) {
+  
+  if (error) {console.log(error);}
+
+
+
+  // x-axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width - 300)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Female Literacy Rate (%)");
+
+  // y-axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Female Labor Force Participation (%)");
+
+
+   var symbolTypes = {
+    "triangleUp": d3.svg.symbol().type("triangle-up"),
+    "circle": d3.svg.symbol().type("circle"),
+    "square": d3.svg.symbol().type("square")
+};
+
+// assign d from our symbols
+   svg.selectAll("path")
+    .data(data)
+    .enter()
+    .append("path")
+    .attr("transform", function(d) { 
+        return "translate(" + xMap(d) +  "," +  yMap(d) + ")"; 
+        
+    })
+    .attr("d", function(d,i){
+        if (d.developState === "High Human Development") {// circle if bar === 0
+            return symbolTypes.circle();
+        }
+        else if (d.developState === "Medium Human Development") {
+            return symbolTypes.triangleUp();
+        }
+        else {
+          return symbolTypes.square();
+        }
+    })
+    .style("fill", color)
+    .style("opacity",function(d){
+      if (d.state == "light") { return 0.3;}
+      else return 1;
+    })
+    ;
+
+var developState = ["High Human Development","Medium Human Development","Low Human Development"];
+
+  // draw legend
+  var highSymbol = d3.svg.symbol().type('circle')(),
+      mediumSymbol = d3.svg.symbol().type('triangle-up')(),
+      lowSymbol = d3.svg.symbol().type('rect')();
+
+  var legend = svg.selectAll(".legend")
+      .data(developState)
+      .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d,i) { return "translate(0," + i * 20 + ")"; });
+
+svg.append("rect")
+      .attr("x", 978)
+      .attr("y",0)
+      .attr("width", 18)
+      .attr("height", 18)
+         .style("stroke", "black")
+	.style("fill", "white");
+     
+
+
+svg.append("polygon")
+    .attr("points", "978,38 996,38 987,20")
+    .attr("x", width)
+    .attr("y",20)
+     .style("stroke", "black")
+	.style("fill", "white");
+
+svg.append("circle")
+      .attr("cx", 987)
+      .attr("cy",49)
+      .attr("r", 9)
+          .style("stroke", "black")
+	.style("fill", "white");
+
+  // draw legend text
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d;})
+
+    // add lables
+    d3.csv("data/mergedData.csv",function(error, data){
+      if (error) {console.log(error);}
+
+      for (var i = data.length - 1; i >= 0; i--) {
+        var country = data[i].Country;
+        var literacyRate = data[i].literacyRate;
+        var laborForce = data[i].LFPrate;
+        //console.log(country);
+
+        svg.append("text")
+        .data(data)
+        .attr("x", function(d){ return data[i].literacyRate * 7 + 5;})
+        .attr("y", function(d){ return 5 * (100 - data[i].LFPrate);})
+        .attr("dy",".35em")
+        .text(function(d){
+          if (data[i].state == "dark") { return data[i].Country;}
+        });
+      }
+
+    });
+
+
+});
